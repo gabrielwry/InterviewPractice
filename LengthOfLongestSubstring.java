0f0c6e242f8237e5ae59bb49c9c0e1f95e7bc649
gabrielwry <runye.wang@emory.edu>
2017-01-16 17:12:21 -0500

    Update LengthOfLongestSubstring.java

--- LengthOfLongestSubstring.java
+++ LengthOfLongestSubstring.java
@@ -1,3 +1,17 @@
+/*Given a string, find the length of the longest substring without repeating characters.
+
+Examples:
+
+Given "abcabcbb", the answer is "abc", which the length is 3.
+
+Given "bbbbb", the answer is "b", with the length of 1.
+
+Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+*/
+/*solution: keep a hashmap of all chars, a start pointer and a end pointer =>
+            pointers only go foward  => if char at end position is larger than
+            start, update start  => update size
+*/ 
 public class Solution {
     public int lengthOfLongestSubstring(String s) {
         HashMap<Character, Integer> pos = new HashMap<Character, Integer>();
