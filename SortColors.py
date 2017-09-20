21366c68626a2b4440905a528b7d074d1a741cec
unknown <runye.wang@emory.edu>
2017-09-20 10:45:00 -0400

    sort color

--- SortColors.py
+++ SortColors.py
@@ -2,6 +2,8 @@
 Given an array with n objects colored red, white or blue, sort them so that objects of the same color are adjacent, with the colors in the order red, white and blue.
 
 Here, we will use the integers 0, 1, and 2 to represent the color red, white, and blue respectively.
+
+Count number of 0,1,2 with inplace one pass sort, do pop and insert: Accepted 44 ms
 """
 
 
