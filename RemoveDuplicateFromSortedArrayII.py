a6ff9610a1fdcca801c429182e1b6bd1c5579d0a
unknown <runye.wang@emory.edu>
2017-09-20 17:57:31 -0400

    remove duplicate from sorted array

---
+++ RemoveDuplicateFromSortedArrayII.py
@@ -0,0 +1,35 @@
+"""
+Follow up for "Remove Duplicates":
+What if duplicates are allowed at most twice?
+
+For example,
+Given sorted array nums = [1,1,1,2,2,3],
+
+Your function should return length = 5, with the first five elements of nums being 1, 1, 2, 2 and 3. It doesn't matter what you leave beyond the new length.
+
+Runs perfectly on local, index out of range error on server
+"""
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        length = 0
+        while len(nums)!= 0:
+            if len(nums) == 1:
+                length += 1
+                break
+
+            i = nums[0]
+            j = nums[1]
+            if i == j:
+                length+=2
+                while len(nums)>0 and nums[0] == i:
+                    nums.remove(i)
+            else:
+                length += 1
+        return length
+
+solution= Solution()
+solution.removeDuplicates([1,1])
